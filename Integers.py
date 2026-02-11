7bdc83c55d1de75beece56dc1ba5bdd293686571
RAMZAN NAVEED <ramzannaveed659@gmail.com>
2026-02-11 11:23:29 +0500

    Add files via upload

---
+++ Integers.py
@@ -0,0 +1,13 @@
+#Integers
+num1 = 10
+num2 = 3
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Power:", num1 ** num2)
+
+
