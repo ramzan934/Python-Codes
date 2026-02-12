955b5f91dd1627e648ad2ee4a38fb246fe081fc9
RAMZAN NAVEED <ramzannaveed659@gmail.com>
2026-02-12 11:16:55 +0500

    Add files via upload

---
+++ Logical Operators.py
@@ -0,0 +1,11 @@
+
+x = 10
+y = 5
+
+print("x > 5 and y > 2:", x > 5 and y > 2)
+print("x > 5 and y > 10:", x > 5 and y > 10)
+
+print("x > 5 or y > 10:", x > 5 or y > 10)
+
+print("not(x > 5):", not(x > 5))
+
